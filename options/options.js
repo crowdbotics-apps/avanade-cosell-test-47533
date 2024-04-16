a293082e61afbe91d7ea5a6ec37caa7a71b7dde5
Crowdbotics <team@crowdbotics.com>
2024-04-16 15:21:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "avanade-cosell-test-47533",
+  url: "https://avanade-cosell-test-47533.botics.co",
+  api: "https://avanade-cosell-test-47533.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
